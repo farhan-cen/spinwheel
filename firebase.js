c1d6cd5ecdf212c8844e40dda77ef7e3a5d6ff31
farhan-cen <fardupoin@gmail.com>
2026-06-11 09:55:37 +0700

    update

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGZD3jwqGGeQyNQ3fuKWLML01jjfut94c",
+  authDomain: "spinwheel-41d0b.firebaseapp.com",
+  databaseURL: "https://spinwheel-41d0b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "spinwheel-41d0b",
+  storageBucket: "spinwheel-41d0b.firebasestorage.app",
+  messagingSenderId: "1037626892232",
+  appId: "1:1037626892232:web:74af568282279185d16afd"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.firebaseDB = db;
+
+console.log("Firebase Connected");
